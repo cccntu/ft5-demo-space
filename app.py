706179c5e216c526e6f8ffaaa265dbdfcf60ee92
Jonathan Chang <ttjtftx@gmail.com>
2021-07-17 19:26:27 +0800

    Add T5, do_sample by default

--- app.py
+++ app.py
@@ -18,14 +18,16 @@ ft5_summarizer = pipeline(
     "summarization", model=ft5_model, tokenizer=ft5_tokenizer, framework="pt"
 )
 
-# model_name = 'flax-community/t5-base-cnn-dm'
-# t5_model = T5ForConditionalGeneration.from_pretrained(model_name)
-# t5_tokenizer = T5TokenizerFast.from_pretrained(model_name)
-# predict_t5 = get_predict(t5_model, t5_tokenizer)
+model_name = "flax-community/t5-base-cnn-dm"
+t5_model = T5ForConditionalGeneration.from_pretrained(model_name)
+t5_tokenizer = T5TokenizerFast.from_pretrained(model_name)
+t5_summarizer = pipeline(
+    "summarization", model=t5_model, tokenizer=t5_tokenizer, framework="pt"
+)
 
 
-def fn(text, do_sample, min_length, max_length, temperature, top_p):
-    out = ft5_summarizer(
+def _fn(text, do_sample, min_length, max_length, temperature, top_p, summarizer):
+    out = summarizer(
         text,
         do_sample=do_sample,
         min_length=min_length,
@@ -37,23 +39,28 @@ def fn(text, do_sample, min_length, max_length, temperature, top_p):
     return out[0]["summary_text"]
 
 
+def fn(*args):
+    return [_fn(*args, summarizer=s) for s in (t5_summarizer, ft5_summarizer)]
+
+
 import gradio as gr
 
 interface = gr.Interface(
     fn,
     inputs=[
-        gr.inputs.Textbox(lines=10, label="text"),
-        gr.inputs.Checkbox(label="do_sample"),
+        gr.inputs.Textbox(lines=10, label="article"),
+        gr.inputs.Checkbox(label="do_sample", default=True),
         gr.inputs.Slider(1, 128, step=1, default=64, label="min_length"),
         gr.inputs.Slider(1, 128, step=1, default=64, label="max_length"),
         gr.inputs.Slider(0.0, 1.0, step=0.1, default=1, label="temperature"),
         gr.inputs.Slider(0.0, 1.0, step=0.1, default=1, label="top_p"),
     ],
-    outputs=gr.outputs.Textbox(),
-    # server_port=8080,
-    # server_name='0.0.0.0',
+    outputs=[
+        gr.outputs.Textbox(label="summary by T5"),
+        gr.outputs.Textbox(label="summary by F-T5"),
+    ],
     examples=[[ex] for ex in examples],
-    title="F-T5 News Summarization",
+    title="F-T5 News Summarizer",
     description="""
 F-T5 is a hybrid encoder-decoder model based on T5 and FNet.
 The model architecture is based on T5, except the encoder self attention is replaced by fourier transform as in FNet.
